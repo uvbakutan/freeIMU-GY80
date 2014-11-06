4cde3375578c0ec2fd1918f6f2395965fa298e68
Halil <uvbakutan@users.noreply.github.com>
2014-11-06 18:17:58 +0900

    Include L3G4200D.h to example, add 4g range for GY80

--- libraries/FreeIMU/examples/FreeIMU_tapdetection/FreeIMU_tapdetection.ino
+++ libraries/FreeIMU/examples/FreeIMU_tapdetection/FreeIMU_tapdetection.ino
@@ -2,6 +2,7 @@
 #include <bma180.h>
 #include <HMC58X3.h>
 #include <ITG3200.h>
+#include <L3G4200D.h>
 #include <MS561101BA.h>
 #include <I2Cdev.h>
 #include <MPU60X0.h>
@@ -23,7 +24,7 @@ bool tap_in_window = 0;
 
 
 // values here are purely empirical
-const int tap_threshold = 12000;
+const int tap_threshold = 120; // changed to 120 original value 120000
 const long tap_duration = 19375;
 
 
@@ -31,32 +32,36 @@ const long tap_duration = 19375;
 FreeIMU my3IMU = FreeIMU();
 
 
-void setup() { 
+void setup() {
   Serial.begin(115200);
   Wire.begin();
-  
+
   delay(10);
   my3IMU.init(true); // the parameter enable or disable fast mode
-  my3IMU.accgyro.setFullScaleAccelRange(1); // set accelerometer to 4g range
+  #if HAS_ITG3200() || HAS_L3G4200D()
+    my3IMU.acc.setRangeSetting(4);
+  #else
+    my3IMU.accgyro.setFullScaleAccelRange(1); // set accelerometer to 4g range
+  #endif
   delay(10);
-  
+
   Serial.println("---\n---\n---\nTap detection starts!");
 }
 
 
 void loop() {
   my3IMU.getRawValues(raw_values);
-  
+
   int a_x = raw_values[0];
-  
+
   if(a_x > tap_threshold && !tap_in_window) {
     tap_window_start = micros();
     tap_in_window = true;
   }
-  
+
   if(tap_in_window) {
     time_in_window = micros() - tap_window_start;
-    
+
     if(a_x < tap_threshold && time_in_window < tap_duration) {
       sprintf(str, "TAP! time: %dus\n", time_in_window);
       Serial.print(str);
@@ -68,4 +73,3 @@ void loop() {
   }
   
 }
-
